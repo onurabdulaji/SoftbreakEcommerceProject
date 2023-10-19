027a2fd861800bc622dbb57d9343fb0e033341ec
Onur Abdulaji <76725341+onurabdulaji@users.noreply.github.com>
2023-10-19 22:40:20 +0200

    # Models Added

---
+++ Project.ENTITIES/Models/AppUserProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+    public class AppUserProfile : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual AppUser AppUser { get; set; }
+    }
+}
